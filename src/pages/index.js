1f0ca51150224ce45e1ef4613695218d642baf1e
maitrakhatri <maitrakhatri@gmail.com>
2023-03-28 13:08:25 +0530

    basic prosemirror setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,26 @@
 import Head from 'next/head'
+import {EditorState} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
+import {Schema, DOMParser} from "prosemirror-model"
+import {schema} from "prosemirror-schema-basic"
+import {addListNodes} from "prosemirror-schema-list"
+import {exampleSetup} from "prosemirror-example-setup"
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    const mySchema = new Schema({
+      nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+      marks: schema.spec.marks
+    })
+    
+    window.view = new EditorView(document.querySelector("#editor"), {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
+        plugins: exampleSetup({schema: mySchema})
+      })
+    })
+  }, [])
   return (
     <>
       <Head>
@@ -10,6 +30,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <div id='editor'></div>
+        <div id='content'></div>
       </main>
     </>
   )
